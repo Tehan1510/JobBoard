b0d40d8dcc273e7773c59bba4489e40d545b1b20
Tehan <tehananuda1510@gmail.com>
2026-05-15 01:45:50 +0530

    update

--- server/server.js
+++ server/server.js
@@ -11,8 +11,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000' }));app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
